b3fefc361477daaa296da99855e2dc062f746249
Dann <46873266+DannArm@users.noreply.github.com>
2021-12-19 22:57:37 +0400

    Update config.js

--- config.js
+++ config.js
@@ -2,14 +2,14 @@ const DEVELOPER = ["382906068319076372"];
 const ADMIN = [...DEVELOPER];
 
 const obj = {
-  TOKEN: "NzI3OTI0OTI5MTY4NjcwNzIw.Xvy66w.Q_Qkp_aYGyJ2QfLaKY2EHejqljk",
-  MONGO: "mongodb+srv://DannTest:099075020@botdiscord.hkvvx.mongodb.net/BotData",
+  TOKEN: process.env.TOKEN || "NzI3OTI0OTI5MTY4NjcwNzIw.Xvy66w.Q_Qkp_aYGyJ2QfLaKY2EHejqljk",
+  MONGO: process.env.MONGO || "mongodb+srv://DannDev:vard04mak@cluster0.fcdo0.mongodb.net/NewBot",
   DEVELOPER,
   ADMIN,
   GLOBAL_COOLDOWN: 1500,
   MAIN_COLOR: "#ffffff",
-  CLIENT_STATUS: "hello baby",
-  SLASH_GUILD: ["839462072970641419"], // or array of guild ids
+  CLIENT_STATUS: "Slash | Dann#1000",
+  SLASH_GUILD: false, // or array of guild ids
   DEFAULT_ITEM_COUNT: 3,
   MAX_BAG_COUNT: 30,
   FIGHT_APPLE_WIN: [10, 30],
