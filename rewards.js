ed47b3e8b33bfd282be6e6fc13ac8279bf92ccc4
Dann <46873266+DannArm@users.noreply.github.com>
2021-12-24 13:32:45 +0400

    Add files via upload

--- rewards.js
+++ rewards.js
@@ -1,4 +1,24 @@
 module.exports = {
   claim: 20,
-  claimApple: 100
+  claimApple: 100,
+  casino: {
+    money1: {
+      cost: 10,
+      min: 7,
+      max: 15,
+      chance: 50
+    },
+    money2: {
+      cost: 50,
+      min: 40,
+      max: 65,
+      chance: 50
+    },
+    money3: {
+      cost: 150,
+      min: 120,
+      max: 180,
+      chance: 50
+    }
+  }
 }
