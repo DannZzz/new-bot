9ebe952852b9541a84236c804e115d0dccfd0a39
Dann <46873266+DannArm@users.noreply.github.com>
2021-12-27 15:33:55 +0400

    Add files via upload

--- utils/database/modelData/server.js
+++ utils/database/modelData/server.js
@@ -2,6 +2,7 @@ const { Schema, model } = require("mongoose");
 
 const newSchema = new Schema({
   _id: String,
+  magic: { type: Boolean, default: false },
 });
 
 module.exports = model("server", newSchema);
