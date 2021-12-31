b895a174b2a6ba53734363541366b6ac1c951e84
Dann <46873266+DannArm@users.noreply.github.com>
2021-12-31 18:51:58 +0400

    Add files via upload

--- slashCommands/information/help.js
+++ slashCommands/information/help.js
@@ -15,70 +15,71 @@ module.exports = {
   run: async (client, int, Data) => {
     const { config, emoji, embed, F, Discord, errEmb, serverData } = Data;
 
-    const cmdName = (int.options.getString("команда")).toLowerCase();
+    var cmdName = int.options.getString("команда");
 
       if (cmdName) {
-      const cmd = client.slashCommands.find(c => c.data.name === cmdName);
-      if (!cmd) return embed(int).setError(`Команда **${cmdName}** не найдена.`).send();
+        cmdName = cmdName.toLowerCase();
+        const cmd = client.slashCommands.find(c => c.data.name === cmdName);
+        if (!cmd) return embed(int).setError(`Команда **${cmdName}** не найдена.`).send();
 
-      const optionTypes = {
-        "1": "Подкоманда",
-        "2": "Подкоманда",
-        "3": "Текст",
-        "4": "Число",
-        "5": "Да/Нет",
-        "6": "Пользователь Дискорда",
-        "7": "Канал",
-        "8": "Роль",
-        "10": "Число"
-      }
-
-      const data = cmd.data;
-      let options;
-      if (data.options && data.options.length > 0) {
-        options = data.options.map((obj, index) => {
-          return `${index+1}. **${obj.name}** - ${optionTypes[obj.type + ""]}\n${obj.required ? emoji.check : emoji.cross}└ ${obj.description}`;
-        })
-      }
-      var commandInGuild = false;
-      if (serverData.disabledCommands) {
-        commandInGuild = serverData.disabledCommands[cmd.name];
-      }
-
-      let disabledRoles, disabledChannels;
-
-      if (commandInGuild) {
-        if (commandInGuild.disabledChannels && commandInGuild.disabledChannels.length > 0) {
-          disabledChannels = commandInGuild.disabledChannels.map(channelId => {
-            const checkData = int.guild.channels.cache.get(channelId)
-            if (checkData) return checkData;
-          });
+        const optionTypes = {
+          "1": "Подкоманда",
+          "2": "Подкоманда",
+          "3": "Текст",
+          "4": "Число",
+          "5": "Да/Нет",
+          "6": "Пользователь Дискорда",
+          "7": "Канал",
+          "8": "Роль",
+          "10": "Число"
         }
-        if (commandInGuild.disabledRoles && commandInGuild.disabledRoles.length > 0) {
-          disabledRoles = commandInGuild.disabledRoles.map(roleId => {
-            const checkData = int.guild.roles.cache.get(roleId)
-            if (checkData) return checkData;
-          });
+
+        const data = cmd.data;
+        let options;
+        if (data.options && data.options.length > 0) {
+          options = data.options.map((obj, index) => {
+            return `${index+1}. **${obj.name}** - ${optionTypes[obj.type + ""]}\n${obj.required ? emoji.check : emoji.cross}└ ${obj.description}`;
+          })
         }
-      }
+        var commandInGuild = false;
+        if (serverData.disabledCommands) {
+          commandInGuild = serverData.disabledCommands[cmd.name];
+        }
 
-      embed(int)
-      .setAuthor("📑 Информация о команде")
-      .setText(stripIndents`
-        **${cmd.data.name}** — ${data.description || "Нет описания"}
+        let disabledRoles, disabledChannels;
 
-        ${emoji.check} - Обязательный параметр
-        ${emoji.cross} - Необязательный параметр
-      `)
-      .addField("# Параметры", stripIndents`
-        ${options && options.length > 0 ? options.join("\n") : "Не найдены"}
-      `)
-      .addField("Глобально отключена:", `${commandInGuild && commandInGuild.globalDisabled === true ? `Да` : `Нет`}`)
-      .addField("Отключённые каналы:", `${disabledChannels && disabledChannels.length > 0 ? `${disabledChannels.join(", ")}` : `Не найдены`}`)
-      .addField("Отключённые роли:", `${disabledRoles && disabledRoles.length > 0 ? `${disabledRoles.join(", ")}` : `Не найдены`}`)
-      .send();
+        if (commandInGuild) {
+          if (commandInGuild.disabledChannels && commandInGuild.disabledChannels.length > 0) {
+            disabledChannels = commandInGuild.disabledChannels.map(channelId => {
+              const checkData = int.guild.channels.cache.get(channelId)
+              if (checkData) return checkData;
+            });
+          }
+          if (commandInGuild.disabledRoles && commandInGuild.disabledRoles.length > 0) {
+            disabledRoles = commandInGuild.disabledRoles.map(roleId => {
+              const checkData = int.guild.roles.cache.get(roleId)
+              if (checkData) return checkData;
+            });
+          }
+        }
 
-      return;
+        embed(int)
+        .setAuthor("📑 Информация о команде")
+        .setText(stripIndents`
+          **${cmd.data.name}** — ${data.description || "Нет описания"}
+
+          ${emoji.check} - Обязательный параметр
+          ${emoji.cross} - Необязательный параметр
+        `)
+        .addField("# Параметры", stripIndents`
+          ${options && options.length > 0 ? options.join("\n") : "Не найдены"}
+        `)
+        .addField("Глобально отключена:", `${commandInGuild && commandInGuild.globalDisabled === true ? `Да` : `Нет`}`)
+        .addField("Отключённые каналы:", `${disabledChannels && disabledChannels.length > 0 ? `${disabledChannels.join(", ")}` : `Не найдены`}`)
+        .addField("Отключённые роли:", `${disabledRoles && disabledRoles.length > 0 ? `${disabledRoles.join(", ")}` : `Не найдены`}`)
+        .send();
+
+        return;
     }
 
     let embeds = [];
