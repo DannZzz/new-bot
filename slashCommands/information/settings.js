0689394d976f6ed9fb08cb697049aa80bb0f0d8f
Dann <46873266+DannArm@users.noreply.github.com>
2022-01-05 19:14:10 +0400

    Add files via upload

--- slashCommands/information/settings.js
+++ slashCommands/information/settings.js
@@ -18,15 +18,29 @@ module.exports = {
       "кикер": "kickAllowedRoles"
     };
 
+    const commands = serverData.disabledCommands || {};
+    const disabled = [];
+
+    for (let commandKey in commands) {
+      const commandData = commands[commandKey];
+      if (commandData.globalDisabled) {
+        const cmd = client.slashCommands.find(data => data.name === commandKey);
+        if (cmd) disabled.push(`\`${cmd.data.name}\``);
+      };
+    };
+
+
     const moderrole = (serverData.moderrole || []).filter(roleId => guild.roles.cache.get(roleId));
     const banAllowedRoles = (serverData.banAllowedRoles || []).filter(roleId => guild.roles.cache.get(roleId));
     const kickAllowedRoles = (serverData.kickAllowedRoles || []).filter(roleId => guild.roles.cache.get(roleId));
 
     const mainEmbed = embed(int)
-    .setAuthor("Настройки сервера")
+    .setAuthor("⚙ Настройки сервера")
     .addField("Роли модераторов:", moderrole.length > 0 ? moderrole.map(roleId => guild.roles.cache.get(roleId)).join(", ") : "Нет назначенных ролей" )
     .addField("Роли банеров:", banAllowedRoles.length > 0 ? banAllowedRoles.map(roleId => guild.roles.cache.get(roleId)).join(", ") : "Нет назначенных ролей" )
     .addField("Роли кикеров:", kickAllowedRoles.length > 0 ? kickAllowedRoles.map(roleId => guild.roles.cache.get(roleId)).join(", ") : "Нет назначенных ролей" )
+    .addField("Глобально отключённые команды:", disabled.length > 0 ? disabled.join(", ") : `Не найдены`)
+    .setFooter("/хелп <команда> — для информации")
     .send();
   }
 }
