7118f66cebb11a48f7296bbdcc1ff9bff58108e7
Dann <46873266+DannArm@users.noreply.github.com>
2022-01-13 22:37:43 +0400

    Add files via upload

--- slashCommands/admin/addSpecialRoles.js
+++ slashCommands/admin/addSpecialRoles.js
@@ -1,5 +1,20 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 
+const msToString = {
+  "5m": "5 минут",
+  "10m": "10 минут",
+  "30m": "30 минут",
+  "1h": "1 час",
+  "2h": "2 часа",
+  "3h": "3 часа",
+  "6h": "6 часов",
+  "12h": "12 часов",
+  "1d": "1 день",
+  "2d": "2 дня",
+  "5d": "5 дней",
+  "1w": "1 неделя"
+}
+
 module.exports = {
   name: "addSpecialRoles",
   category: 2,
@@ -47,6 +62,23 @@ module.exports = {
       .setDescription("Количество доступных мьютов в день.")
       .setRequired(true)
     )
+    .addStringOption(o => o
+      .setName("максимальный-срок")
+      .setDescription("Назначить для этой роли максимальный срок выдачи мьюта. ")
+      .addChoice("5 минут", "5m")
+      .addChoice("10 минут", "10m")
+      .addChoice("30 минут", "30m")
+      .addChoice("1 час", "1h")
+      .addChoice("2 часа", "2h")
+      .addChoice("3 часа", "3h")
+      .addChoice("6 часов", "6h")
+      .addChoice("12 часов", "12h")
+      .addChoice("1 день", "1d")
+      .addChoice("2 дня", "2d")
+      .addChoice("5 дней", "5d")
+      .addChoice("1 неделя", "1w")
+
+    )
   )
   ,
   run: async (client, int, Data) => {
@@ -57,6 +89,7 @@ module.exports = {
     const dataType = int.options.getSubcommand();
     const role = int.options.getRole("роль");
     const uses = int.options.getInteger("количество");
+    const limit_for_mute = int.options.getString("максимальный-срок") || undefined;
 
     if (!role) embed(int).setError("Роль не найдена!").send();
     let obj = {
@@ -83,16 +116,16 @@ module.exports = {
         if (thisRoleIndex < 0 && uses === 0) {
           return embed(int).setError(`Роль ${role} и так не имеет плюшек.`).send();
         } else if (thisRoleIndex < 0) {
-          await db.models.server.updateOne({_id: guild.id}, {$set: {temporaryRolesForMute: [...serverData.temporaryRolesForMute, {id: role.id, uses}]} });
-          return embed(int).setSuccess(`Участники с ролью ${role} теперь могут замьютить участников ниже, **${uses}** раз(-а) в день.`).send();
+          await db.models.server.updateOne({_id: guild.id}, {$set: {temporaryRolesForMute: [...serverData.temporaryRolesForMute, {id: role.id, uses, limit: limit_for_mute}]} });
+          return embed(int).setSuccess(`Участники с ролью ${role} теперь могут замьютить участников ниже, **${uses}** раз(-а) в день, а максимальный срок: **${limit_for_mute ? msToString[limit_for_mute] : msToString["1w"]}**.`).send();
         } else {
           if (uses === 0) {
             serverData.temporaryRolesForMute.splice(thisRoleIndex, 1);
             await db.models.server.updateOne({_id: guild.id}, {$set: {temporaryRolesForMute: serverData.temporaryRolesForMute} });
             return embed(int).setSuccess(`Роль ${role} успешно убрана.`).send();
           } else {
-            await db.models.server.updateOne({_id: guild.id}, {$set: {[`temporaryRolesForMute.${thisRoleIndex}.uses`]: uses} });
-            return embed(int).setSuccess(`Участники с ролью ${role} теперь могут замьютить участников ниже, **${uses}** раз(-а) в день.`).send();
+            await db.models.server.updateOne({_id: guild.id}, {$set: {[`temporaryRolesForMute.${thisRoleIndex}.uses`]: uses, [`temporaryRolesForMute.${thisRoleIndex}.limit`]: limit_for_mute} });
+            return embed(int).setSuccess(`Участники с ролью ${role} теперь могут замьютить участников ниже, **${uses}** раз(-а) в день, а максимальный срок: **${limit_for_mute ? msToString[limit_for_mute] : msToString["1w"]}**.`).send();
           }
         }
       } else {
@@ -100,7 +133,7 @@ module.exports = {
           return embed(int).setError(`Роль ${role} и так не имеет плюшек.`).send();
         }
         serverData.temporaryRolesForMute = [];
-        serverData.temporaryRolesForMute.push({id: role.id, uses});
+        serverData.temporaryRolesForMute.push({id: role.id, uses, limit: limit_for_mute});
         await serverData.save();
         return embed(int).setSuccess(`Участники с ролью ${role} теперь могут замьютить участников ниже, **${uses}** раз в день.`).send();
       }
