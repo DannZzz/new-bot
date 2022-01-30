5d7982d699ea9c4a0558e45579db25f0848ae23a
Dann <46873266+DannArm@users.noreply.github.com>
2022-01-30 17:48:45 +0400

    Add files via upload

--- utils/database/modelData/server.js
+++ utils/database/modelData/server.js
@@ -3,8 +3,6 @@ const { Schema, model } = require("mongoose");
 const newSchema = new Schema({
   _id: String,
   premium: { type: Date, default: undefined },
-  magic: { type: Boolean, default: false },
-  magicDisabledChannels: { type: Array, default: [] },
   moderrole: { type: Array, default: [] },
   banAllowedRoles: { type: Array, default: [] },
   kickAllowedRoles: { type: Array, default: [] },
